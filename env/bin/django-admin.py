d7d79e2f3fbe2565817fd773e6dea07dd99fd2ed
wuchunlong0 <wcl6005@163.com>
2019-04-22 13:07:35 +0800

    aass

--- env/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/wuchunlong/local/github/abbytraining/Controller/complete/uk_upfile_down/env/bin/python3.5
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
