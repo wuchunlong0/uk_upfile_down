3532e24deb57d7394fa96cd0562144ca68d9e506
wuchunlong0 <wcl6005@163.com>
2019-04-22 13:03:55 +0800

    del db

---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/wuchunlong/local/github/abbytraining/Controller/complete/uk_upfile_down/env/bin/python3.5
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
